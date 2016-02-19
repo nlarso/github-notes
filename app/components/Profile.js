f9a3be48cb0c440fcfc95f0474e19c665169adb5
nlarso <nlarso@gmail.com>
2016-02-19 14:51:04 -0700

    Begin adding basic components
    This commit adds a Profile, UserProfile, Repos and Notes components.
    They are are displaying placeholder data for the moment.

---
+++ app/components/Profile.js
@@ -0,0 +1,36 @@
+var React = require('react');
+var Router = require('react-router');
+var Repos = require('./github/Repos');
+var UserProfile = require('./github/UserProfile');
+var Notes = require('./notes/Notes');
+
+var Profile = React.createClass({
+  getInitialState: function() {
+    return {
+      notes: [],
+      bio: {
+        name: 'Mart McFly'
+      },
+      repos: []
+    }
+  },
+
+
+  render: function(){
+    return(
+      <div className="row">
+        <div className="col-md-4">
+          <UserProfile username={this.props.params.username} bio={this.state.bio} />
+        </div>
+        <div className="col-md-4">
+          <Repos repos={this.state.repos} />
+        </div>
+        <div className="col-md-4">
+          <Notes notes={this.state.notes} />
+        </div>
+      </div>
+    )
+  }
+});
+
+module.exports = Profile;
