6071bb0fe0a6a246c2476819e0b31680bcbdf16b
nlarso <nlarso@gmail.com>
2016-02-21 11:49:19 -0700

    Add SearchGithubComponent, WIP
    This commit adds a SearchGithub component. I marked it as work in
    progress because I have not implemented the integration with Github yet.

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,12 +1,13 @@
 var React = require('react');
+var SearchGithub = require('./SearchGithub');
 
 var Main = React.createClass({
   render: function(){
     return (
       <div className="main-container">
         <nav className="navbar navbar-default" role="navigation">
-          <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-            MENU
+          <div className="col-sm-8 col-sm-offset-2" style={{marginTop: 15}}>
+            <SearchGithub />
           </div>
         </nav>
         <div className="container">
